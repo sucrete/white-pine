cd1a6850c993f0a4d62b281f5809ba2a2614d2b7
Will Andree <williamandree@gmail.com>
2025-06-20 11:53:41 -0500

    Fixed event ordering on events list on home page.

--- assets/js/fetchData.js
+++ assets/js/fetchData.js
@@ -1,10 +1,8 @@
 console.log("fetchData attached");
-  const sanityQuery =
-        "https://5vgxmbfr.api.sanity.io/v2025-06-12/data/query/production?query=%7B%27events%27%3A*%5B_type+%3D%3D+%27event%27%5D%2C%27images%27%3A*%5B_type+%3D%3D+%22sanity.imageAsset%22%5D%7D&perspective=published";
 
-async function fetchData() {
+async function fetchData(query) {
   try {
-    const response = await fetch(sanityQuery);
+    const response = await fetch(query);
 
     if (!response.ok) {
       throw new Error(`Network response was not ok: ${response.status}`);
@@ -12,7 +10,12 @@ async function fetchData() {
 
     const data = await response.json();
     // console.log("%cfetch successful ", "color:chartreuse;background: black");
-    console.log("%cfetch successful ", "color:chartreuse;background: black", data, data.result);
+    console.log(
+      "%cfetch successful ",
+      "color:chartreuse;background: black",
+      data,
+      data.result
+    );
     return data.result;
   } catch (error) {
     console.error("Error fetching data:", error);
--- assets/js/listBuilder.js
+++ assets/js/listBuilder.js
@@ -1,19 +1,11 @@
 const calendarQuery =
   "https://5vgxmbfr.api.sanity.io/v2025-06-12/data/query/production?query=%7B%27events%27%3A*%5B_type+%3D%3D+%27event%27%5D%2C%27images%27%3A*%5B_type+%3D%3D+%22sanity.imageAsset%22%5D%7D&perspective=published";
 
-document.addEventListener("DOMContentLoaded", function () {
-  var cal = document.getElementById("calendar");
-  // var myModal = document.getElementById("calModal");
-  var modalTitle = document.getElementsByClassName("modal-title")[0];
-  var modalBody = document.getElementsByClassName("modal-body")[0];
-  var modalDate = document.getElementsByClassName("modal-date")[0];
-  var modalDateEnd = document.getElementsByClassName("modal-date-end")[0];
-  var modalBodyWrapper =
-    document.getElementsByClassName("modal-body-wrapper")[0];
-  var modalFooter = document.getElementsByClassName("modal-footer")[0];
+const todaysDate = new Date();
+const futureDate = todaysDate.setMonth(todaysDate.getMonth() + 5);
 
-  var modalAttachment = document.getElementsByClassName("modal-attachment")[0];
-  var modalLink = document.getElementsByClassName("modal-link")[0];
+document.addEventListener("DOMContentLoaded", function () {
+  var cal = document.getElementById("events-list");
 
   //~ fetch data
   fetchData(calendarQuery).then((data) => {
@@ -22,14 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const CustomViewConfig = {
       classNames: ["custom-view"],
       content: function (props) {
-        let segs = sliceEvents(props, true); // allDay=true
-        console.log("segs", segs);
-        console.log("segs.length < 0", segs.length > 0);
+        let segs = sliceEvents(props, true);
+        const sortedSegs = segs.sort(
+          (a, b) => new Date(b.range.start) - new Date(a.range.start)
+        );
+        console.log("sorted segs", sortedSegs);
+        console.log("segs.length > 0?: ", segs.length > 0);
         console.log("segs[0].def", segs[0]?.def);
         let html =
           segs.length > 0
             ? ` <div class="events-list-wrapper d-flex flex-column-reverse">
-                  ${segs
+                  ${sortedSegs
+                    .slice(-3)
                     .map((seg) => {
                       return `
                   <div class="event-list-item">
@@ -88,11 +84,8 @@ document.addEventListener("DOMContentLoaded", function () {
                   `
                      : ""
                  }
-               
-               
               </div>
                   </div>
-                
               `;
                     })
                     .join("")}
@@ -116,78 +109,21 @@ document.addEventListener("DOMContentLoaded", function () {
     //~ initialize Calendar
     var calendar = new FullCalendar.Calendar(cal, {
       plugins: [CustomViewPlugin],
-      headerToolbar: {
-        left: "dayGridMonth,monthList",
-        center: "title",
-        right: "today prev,next",
-      },
+      headerToolbar: false,
       views: {
         monthList: {
-          buttonText: "list",
-          duration: {
-            months: 1,
+          // duration: {
+          //   months: 1,
+          // },
+          visibleRange: {
+            start: new Date(),
+            end: futureDate,
           },
-          // contentHeight: '500px',
           height: "unset",
         },
       },
-      initialView: "dayGridMonth",
+      initialView: "monthList",
       events: data.events,
-
-      eventClick: function (info) {
-        //~Modal
-        // ➡️ add title
-        modalTitle.textContent = info.event.title;
-
-        // ➡️ add start date
-        modalDate.textContent = moment(info.event.start).format(
-          "dddd, MMMM Do"
-        );
-        // ➡️ add event description
-        modalBody.textContent = info.event.extendedProps.eventDescription;
-
-        // ➡️ check if there is an event end and if not remove any formally set .textContent
-        if (info.event.end) {
-          modalDateEnd.textContent =
-            " - " +
-            moment(info.event.end).subtract(1, "days").format("dddd, MMMM Do");
-        } else {
-          modalDateEnd.textContent = "";
-        }
-
-        // ➡️  check if there is an attachment AND a link
-        if (
-          info.event.extendedProps.flyerQuestion ||
-          info.event.extendedProps.linkQuestion
-        ) {
-          modalFooter.style = "display: flex";
-          modalBodyWrapper.style = "padding-bottom: 1rem";
-          // ➡️ if there is a flyer, search for it and attach it to the View Flyer button...
-          if (info.event.extendedProps.flyerQuestion) {
-            modalAttachment.style = "display:flex";
-            var imageAssetObject = data.images.find(
-              (image) => image._id === info.event.extendedProps.flyer.asset._ref
-            );
-            modalAttachment.href = imageAssetObject.url;
-          } else {
-            // ...otherwise delete the button
-            modalAttachment.style = "display:none";
-          }
-          // ➡️  if there is a registration link, search for it and attach it to the View Flyer button...
-          if (info.event.extendedProps.linkQuestion) {
-            modalLink.style = "display:flex";
-            modalLink.href = info.event.extendedProps.linkDeets.linkURL;
-          } else {
-            // ...otherwise delete the button
-            modalLink.style = "display:none";
-          }
-        } else {
-          modalFooter.style = "display: none";
-          modalBodyWrapper.style = "padding-bottom: 4rem";
-        }
-
-        $("#calModal").modal("show");
-      },
     });
     calendar.render();
   });
